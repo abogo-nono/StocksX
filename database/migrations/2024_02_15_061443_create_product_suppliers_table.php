c1310b747aef8a27d31ec1b10a0aee9d5021cfef
Abogo <abogonono1@gmail.com>
2024-04-12 14:31:01 +0100

    category column added to the product supplier table

--- database/migrations/2024_02_15_061443_create_product_suppliers_table.php
+++ database/migrations/2024_02_15_061443_create_product_suppliers_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string("name");
             $table->string("email");
             $table->string("phone");
+            $table->string("category");
             $table->timestamps();
             $table->softDeletes();
         });
