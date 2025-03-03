daac0c86df632f2f706c092dee19e255f1d95d4b
ABOGO <abogonono1@gmail.com>
2025-03-03 10:58:31 +0100

    deployment script updated to install require is missed

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
+set -e
+
 echo "|=======================================================|"
 echo "|                                                       |"
-echo "|                 StocksX Deployement script v1.0.0     |"
+echo "|                 StocksX Deployment script v1.0.1      |"
 echo "|                                                       |"
-echo "|                                      By Abogo Lincoln |"
+echo "|                                      By ABOGO Lincoln |"
 echo "|=======================================================|"
 
 # checking for requirement
 echo "||------------------------------------------------------------------"
 echo "||====== Requirement dependencies"
 echo "||------------------------------------------------------------------"
+echo "Checking PHP extensions..."
+required_extensions=("pdo_mysql" "mbstring" "tokenizer" "xml" "ctype" "json" "bcmath" "openssl" "curl" "fileinfo" "gd" "intl")
+for extension in "${required_extensions[@]}"; do
+    if php -m | grep -q "$extension"; then
+        echo "$extension is installed."
+    else
+        echo "$extension is not installed. Installing..."
+        sudo apt-get install -y php-"$extension"
+    fi
+done
 echo
 echo "[Your php version]"
-`php --version`
+php --version
 echo "[Your mysql version]"
-`mysql --version`
+mysql --version
 echo "[Your node version]"
-`node --version`
+node --version
 echo "[Your npm version]"
-`npm --version`
+npm --version
 echo "[Your composer version]"
-`composer --version`
+composer --version
 echo
 
 # cloning the repo
@@ -52,7 +64,7 @@ npm install
 echo "||------------------------------------------------------------------"
 echo "||====== Configuring environment variables"
 echo "||------------------------------------------------------------------"
-mv .env.example .env
+cp .env.example .env
 
 # generate app key
 echo "||------------------------------------------------------------------"
@@ -62,11 +74,11 @@ php artisan key:generate
 
 # migrate, seed database and generate roles
 echo "||------------------------------------------------------------------"
-echo "||====== Migrate, Seed database and generatre roles and permission"
+echo "||====== Migrate, Seed database and generate roles and permissions"
 echo "||------------------------------------------------------------------"
 php artisan migrate --seed
 
-# create a the super admain user
+# create the super admin user
 php artisan make:filament-user --name=Admin --email=admin@example.com --password=12345
 php artisan shield:super-admin --user=1
 
@@ -84,7 +96,7 @@ echo "||====== linking the storage folder"
 echo "||------------------------------------------------------------------"
 php artisan storage:link
 
-# generating politicise for all resources and first user role
+# generating policies for all resources and first user role
 echo "||------------------------------------------------------------------"
 echo "||====== Generate roles"
 echo "||------------------------------------------------------------------"
@@ -93,10 +105,6 @@ php artisan shield:install --fresh
 echo "||------------------------------------------------------------------"
 echo "||====== Serving the app"
 echo "||------------------------------------------------------------------"
-echo "Username: admin@example.com"
-echo "Password: 12345678"
-echo
-echo "By default this user will be the superadmin on the app"
 php artisan serve
 
 # By Abogo Lincoln
