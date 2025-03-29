8d08c33f6f8e94ac49e61b89cbbc5af8d88955bb
ABOGO <abogonono1@gmail.com>
2025-03-29 04:45:08 +0100

    feat: Enable edit action in ViewOrder page header
    - Added `Actions\EditAction::make()` to the `getHeaderActions` method, allowing users to edit orders directly from the ViewOrder page.
    - Retained the delete action for removing orders.
    
    This update improves the functionality of the ViewOrder page by providing quick access to the edit feature.

--- app/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -13,7 +13,7 @@ class ViewOrder extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\EditAction::make(),
+            Actions\EditAction::make(),
             Actions\DeleteAction::make(),
         ];
     }
