78280a0eacd7c2d1a9a0017724f1f9d8eded43a3
ABOGO <abogonono1@gmail.com>
2025-04-04 05:05:33 +0100

    feat: Enhance OrderFactory to include realistic order data generation

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,14 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => $this->faker->numberBetween(1, 3),
+            'client_name' => $this->faker->name(),
+            'client_phone' => $this->faker->phoneNumber(),
+            'client_address' => $this->faker->address(),
+            'total' => $this->faker->randomFloat(2, 1, 1000),
+            'delivered' => $this->faker->boolean(),
+            'created_at' => Carbon::now()->startOfYear()->addDays(rand(0, 364)), // Random date within the current year
+            'updated_at' => Carbon::now()->startOfYear()->addDays(rand(0, 364)), // Optional: Match updated_at to created_at
         ];
     }
 }
