df134fdbcbddca8a91ee40ff6308ea96d55ef2db
ABOGO <abogonono1@gmail.com>
2025-04-04 05:05:49 +0100

    feat: Enhance ProductFactory to generate unique product names and slugs

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -16,8 +17,15 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $name  = $this->faker->unique()->word();
         return [
-            //
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'image' => $this->faker->imageUrl(640, 480, 'products', true),
+            'price' => $this->faker->randomFloat(2, 1, 1000),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'product_categories_id' => $this->faker->numberBetween(1, 5),
+            'product_suppliers_id' => $this->faker->numberBetween(1, 3),
         ];
     }
 }
