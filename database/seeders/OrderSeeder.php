92715bae5a7031157ba023955289caabb586f063
ABOGO <abogonono1@gmail.com>
2025-04-04 05:04:44 +0100

    feat: Truncate Order table and seed with factory data

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Order::truncate()->cascade();
+        Order::factory(40)->create();
     }
 }
